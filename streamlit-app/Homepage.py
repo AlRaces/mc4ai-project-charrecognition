824f1d5c27fec2d9630cbf1295fabbe024f4c708
Hy-LeTuan <letuanhy1507@gmail.com>
2023-05-10 16:16:54 +0700

    Update UI and homepage'

---
+++ streamlit-app/Homepage.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+# cho người dùng chọn epoch, test size, train size 
+# có 3 cái slidebar cho epoch, test size, train size. Khi nào người dùng chọn đủ 3 cái thì bấm nút submit, sau đó split train, test theo số lượng của người dùng rồi train model dựa trên epoch
+
+"----"
+st.sidebar.title("Select Page")
+
+st.markdown(
+    """
+    <h1 style='text-align: center;'>Project Homepage</h1>
+    """,
+    unsafe_allow_html=True,
+)
+
+"----"
+with st.container(): 
+    st.subheader("**Concept**") 
+    st.write("Lorem ipsum")
+
+
+"----"
+with st.container(): 
+    st.subheader("_About us_ :sunglasses:")
+    
--- streamlit-app/app.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-# cho người dùng chọn epoch, test size, train size 
-# có 3 cái slidebar cho epoch, test size, train size. Khi nào người dùng chọn đủ 3 cái thì bấm nút submit, sau đó split train, test theo số lượng của người dùng rồi train model dựa trên epoch
-
