33490884bbe6c135b64cb46dd5f784df76b23b4d
Hy-LeTuan <letuanhy1507@gmail.com>
2023-05-13 16:41:26 +0700

    Bug fix

--- streamlit-app/Homepage.py
+++ streamlit-app/Homepage.py
@@ -4,7 +4,6 @@ import streamlit as st
 # cho người dùng chọn epoch, test size, train size
 # có 3 cái slidebar cho epoch, test size, train size. Khi nào người dùng chọn đủ 3 cái thì bấm nút submit, sau đó split train, test theo số lượng của người dùng rồi train model dựa trên epoch
 
-"----"
 st.sidebar.title("Select Page")
 
 st.markdown(
